c6e9eb00f8f95de201fb88829d0d7c58147763ee
Rushikesh Shinde <shinderushikesh141@gmail.com>
2023-12-08 15:33:23 +0530

    Changes | setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Clone the repository
-git clone https://github.com/rushikesh-shinde-pyd/TradingProject.git .
-
-# Create and activate virtual environment
-virtualenv -p python3.9 venv
-source venv/bin/activate
-
 # Upgrade pip and install requirements
 pip install --upgrade pip
 pip install -r requirements.txt
@@ -15,8 +8,8 @@ pip install -r requirements.txt
 mkdir media
 
 # Run Django management commands
-python manage.py check
 python manage.py makemigrations
+python manage.py migrate
 
 # Run the ASGI application using uvicorn
 uvicorn TradingProject.asgi:application --reload
